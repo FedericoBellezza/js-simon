725f591cc8a2dd4f10aa237a2a07087dd3447007
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-10-24 15:06:17 +0200

    add timout and finish function

--- js/main.js
+++ js/main.js
@@ -6,38 +6,70 @@ const thirdSlotEl = document.getElementById('third-slot');
 const fourthSlotEl = document.getElementById('fourth-slot');
 const fifthSlotEl = document.getElementById('fifth-slot');
 const submitButtonEl = document.getElementById('submit-button');
+let numbers = [];
+let countdown = 3;
 
 // - Game start function
 const gameStart = () => {
-	// - SLOT 1 (Assegno un numero per ogni slot)
+	countdown = 3;
+	countdownEl.innerText = countdown;
+	numbers = [];
+	// - SLOT 1 (Assegno un numero per ogni slot e lo aggiungo ad un array)
 	let currentNumber = randomNumGeneration(1, 99);
 	firstSlotEl.setAttribute('value', currentNumber);
-	console.log(currentNumber);
-
-	// - SLOT 2 (Assegno un numero per ogni slot)
+	numbers.push(currentNumber);
+	// - SLOT 2 (Assegno un numero per ogni slot e lo aggiungo ad un array)
 	currentNumber = randomNumGeneration(1, 99);
 	secondSlotEl.setAttribute('value', currentNumber);
-	console.log(currentNumber);
-
-	// - SLOT 3 (Assegno un numero per ogni slot)
+	numbers.push(currentNumber);
+	// - SLOT 3 (Assegno un numero per ogni slot e lo aggiungo ad un array)
 	currentNumber = randomNumGeneration(1, 99);
 	thirdSlotEl.setAttribute('value', currentNumber);
-	console.log(currentNumber);
-
-	// - SLOT 4 (Assegno un numero per ogni slot)
+	numbers.push(currentNumber);
+	// - SLOT 4 (Assegno un numero per ogni slot e lo aggiungo ad un array)
 	currentNumber = randomNumGeneration(1, 99);
 	fourthSlotEl.setAttribute('value', currentNumber);
-	console.log(currentNumber);
-
-	// - SLOT 5 (Assegno un numero per ogni slot)
+	numbers.push(currentNumber);
+	// - SLOT 5 (Assegno un numero per ogni slot e lo aggiungo ad un array)
 	currentNumber = randomNumGeneration(1, 99);
 	fifthSlotEl.setAttribute('value', currentNumber);
-	console.log(currentNumber);
+	numbers.push(currentNumber);
+
+	// - Passing seconds function
+	const passingSecond = () => {
+		// - Countdown Finish
+		if (countdown === 1) {
+			timeOut();
+			clearInterval(Interval);
+		} else {
+			countdown--;
+			countdownEl.innerText = countdown;
+		}
+	};
+
+	// - Timeout start
+	const Interval = setInterval(passingSecond, 1000);
 };
 
 // - Random number generation function
 function randomNumGeneration(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
+
+const timeOut = () => {
+	firstSlotEl.setAttribute('value', '');
+	secondSlotEl.setAttribute('value', '');
+	thirdSlotEl.setAttribute('value', '');
+	fourthSlotEl.setAttribute('value', '');
+	fifthSlotEl.setAttribute('value', '');
+
+	countdownEl.innerText = 'Inserisci i numeri che ti ricordi';
+
+	firstSlotEl.removeAttribute('readonly');
+	secondSlotEl.removeAttribute('readonly');
+	thirdSlotEl.removeAttribute('readonly');
+	fourthSlotEl.removeAttribute('readonly');
+	fifthSlotEl.removeAttribute('readonly');
+};
 
 submitButtonEl.addEventListener('click', gameStart);
